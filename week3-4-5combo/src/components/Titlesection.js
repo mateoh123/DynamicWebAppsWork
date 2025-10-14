2794d268ff63d8d3099377a8f534a6fedaa0ea86
mateoh123 <mateoche76@gmail.com>
2025-10-14 01:54:32 -0400

    custom component

--- week3-4-5combo/src/components/Titlesection.js
+++ week3-4-5combo/src/components/Titlesection.js
@@ -1,23 +1,15 @@
-import cx from "classnames";
-import PropTypes from "prop-types";
-
-const TitleSection = () => {
+const TitleSection = (props) => {
+  const { image, title, description } = props;
   return (
-    <>
-      <div className="flex justify-center">
-        <img
-          src={require("../assets/myhead.png")}
-          alt="''"
-          className="h-96 w-96"
-        ></img>
-        <div>
-          <h1 className="text-[10rem] flex ">Mateo</h1>
-          <p className="text-[2rem]">
-            Description Text; Portfolio Section Info
-          </p>
-        </div>
+    <div className="flex justify-center items-center gap-8">
+      <img src={image} alt={title} className="h-96 w-96 object-cover" />
+      <div>
+        <h1 className="text-[10rem] leading-none font-bold">{title}</h1>
+        <p className="text-[2rem] bg-gray-200 px-5 py-2 inline-block mt-4">
+          {description}
+        </p>
       </div>
-    </>
+    </div>
   );
 };
 
--- week3-4-5combo/src/pages/HomePage.js
+++ week3-4-5combo/src/pages/HomePage.js
@@ -4,7 +4,16 @@ import TitleSection from "../components/Titlesection";
 const HomePage = () => {
   return (
     <div>
-      <TitleSection />
+      <TitleSection
+        title="Mateo"
+        description="Description Text; Portfolio Section Info"
+        image={require("../assets/myhead.png")}
+      />
+      <TitleSection
+        title="Mateo 2"
+        description="Description Text; Version 2 Lol"
+        image={require("../assets/myhead.png")}
+      />
     </div>
   );
 };
